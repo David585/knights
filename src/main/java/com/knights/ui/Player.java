369693df5122ecfa64670e99a219059051cd38ae
wynnka <wynnka@10.1.1.130>
2015-01-23 09:19:33 +0000

    Create health and damage variables, also created setters and getters

--- src/main/java/com/knights/ui/Player.java
+++ src/main/java/com/knights/ui/Player.java
@@ -13,6 +13,25 @@ public class Player implements Serializable {
  
 	
 	protected String name;
+	protected int health = 100;
+	protected int damage = 60;
+	
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
 
 	public String getName() {
 		return name;
