b37d7c5ead97f23f5924843a4469c81d398cbf85
wynnka <wynnka@10.1.1.115>
2015-01-28 14:48:14 +0000

    init

--- src/main/java/com/knights/ui/Player.java
+++ src/main/java/com/knights/ui/Player.java
@@ -1,45 +1,9 @@
 package com.knights.ui;
 
-import java.io.Serializable;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-
-@ManagedBean
-@SessionScoped
-public class Player implements Serializable {
-
-	private static final long serialVersionUID = 6666839705272565405L;
- 
+public final class Player extends GameEntity {
 	
-	protected String name;
-	protected int health = 100;
-	protected int damage = 60;
-	
-	
-
-	public int getHealth() {
-		return health;
+	@Override
+	public void consume(Food food) {
+		System.out.println("Player class impl");
 	}
-
-	public void setHealth(int health) {
-		this.health = health;
-	}
-
-	public int getDamage() {
-		return damage;
-	}
-
-	public void setDamage(int damage) {
-		this.damage = damage;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
 }
---
+++ src/main/java/com/knights/ui/old/Player.java
@@ -0,0 +1,45 @@
+package com.knights.ui.old;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class Player implements Serializable {
+
+	private static final long serialVersionUID = 6666839705272565405L;
+ 
+	
+	protected String name;
+	protected int health = 100;
+	protected int damage = 60;
+	
+	
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
